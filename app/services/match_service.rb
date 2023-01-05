3eea341145e6bbb601b1f60c5269a67248c8927a
Matt Goggin <goggin13@gmail.com>
2023-01-05 16:17:08 -0600

    predicted score

--- app/services/match_service.rb
+++ app/services/match_service.rb
@@ -10,6 +10,27 @@ class MatchService
       acc
     end
 
-    {home: home_data, away: away_data}
+    home_score = 0
+    away_score = 0
+    WrestlerService::WEIGHTS.each do |weight|
+      if home_data.has_key?(weight) && away_data.has_key?(weight)
+        if home_data[weight].rank < away_data[weight].rank
+          home_score += 3
+        else
+          away_score += 3
+        end
+      elsif home_data.has_key?(weight)
+        home_score += 4
+      else
+        away_score += 4
+      end
+    end
+
+    {
+      home: home_data,
+      away: away_data,
+      home_score: home_score,
+      away_score: away_score
+    }
   end
 end
