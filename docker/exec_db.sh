a74bc584d8f12bce99aa542c0985eb0bf22fac23
Matt Goggin <goggin13@gmail.com>
2023-01-10 17:43:43 -0600

    fix exec_db command

--- docker/exec_db.sh
+++ docker/exec_db.sh
@@ -1,4 +1,4 @@
-if docker ps | grep -o dumbledore-database_1 ; then
+if docker ps | grep -o dumbledore_database_1 ; then
   docker exec -it dumbledore_database_1 bash
 else
   echo "run ./docker/start_app.sh first"
