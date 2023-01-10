e8faaa151bd46d681cb45e14621b65066a0f5a8c
Matt Goggin <goggin13@gmail.com>
2023-01-10 17:49:31 -0600

    only look 1 day back

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -3,7 +3,7 @@ class MatchesController < ApplicationController
 
   def index
     @matches = Match
-      .where("date >= ?", Date.today.in_time_zone("Central Time (US & Canada)") - 1.days)
+      .where("date >= ?", Date.today.in_time_zone("Central Time (US & Canada)") - 2.days)
       .order(:date).order(:time).order(:watch_on)
       .all
 
