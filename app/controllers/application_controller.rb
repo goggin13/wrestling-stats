188daddb91d2abc678c66475eeb55c9abd031a47
Matt Goggin <goggin13@gmail.com>
2023-02-04 08:36:23 -0600

    CORS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   skip_before_action :authenticate_admin!, if: :devise_controller?
   skip_before_action :authenticate_admin!, only: [:access_denied]
 
+  before_action :cors_preflight_check
+  after_action :cors_set_access_control_headers
+
   def authenticate_admin!
     authenticate_user!
     unless current_user.email == "goggin13@gmail.com"
@@ -12,5 +15,18 @@ class ApplicationController < ActionController::Base
   end
 
   def access_denied
+  end
+
+  def cors_set_access_control_headers
+    headers["Access-Control-Allow-Origin"] = "*"
+    headers["Access-Control-Allow-Methods"] = "POST, PUT, DELETE, GET, OPTIONS"
+    headers["Access-Control-Allow-Headers"] = "*"
+    headers["Access-Control-Max-Age"] = "1728000"
+    headers["Access-Control-Allow-Credentials"] = "true"
+  end
+
+  def cors_preflight_check
+    return unless request.method == :options
+    render :text => "", :content_type => "text/plain"
   end
 end
