0c8438e63a76ececd6625b789959d5b30caba582
Matt Goggin <goggin13@gmail.com>
2023-11-19 18:06:04 -0600

    Wrestling rankings from flow

--- app/services/wrestler_service.rb
+++ app/services/wrestler_service.rb
@@ -1,27 +1,52 @@
 class WrestlerService
+  FLOW_ROOT = "https://www.flowrestling.org"
+
   WEIGHTS = [125, 133, 141, 149, 157, 165, 174, 184, 197, 285]
 
   def self.scrape_rankings
+    cached_urls = ranking_urls_by_weight
     WEIGHTS.each do |weight|
-      WrestlerService.scrape_rankings_for_weight(weight)
+      WrestlerService.scrape_rankings_for_weight(weight, cached_urls)
     end
-    WrestlerService.scrape_team_dual_rankings
-    WrestlerService.scrape_team_tournament_rankings
+    # WrestlerService.scrape_team_dual_rankings
+    WrestlerService.scrape_team_tournament_rankings(cached_urls)
   end
 
-  def self.scrape_rankings_for_weight(weight)
+  def self.current_rankings_url
+    flow_rankings_index_url = "#{FLOW_ROOT}/rankings"
+    document = Nokogiri::HTML(DownloadService.download(flow_rankings_index_url))
+    path = document.xpath("//a[contains(., '2023-24 NCAA DI Rankings')]")[0]["href"]
+
+    FLOW_ROOT + path
+  end
+
+  def self.ranking_urls_by_weight
+    doc = Nokogiri::HTML(DownloadService.download(current_rankings_url))
+
+    path = doc.xpath("//a[contains(., 'Team Tournament')]")[0]["href"]
+    results = {tournament: FLOW_ROOT + path}
+
+    WEIGHTS.inject(results) do |acc, weight|
+      path = doc.xpath("//a[contains(., '#{weight}')]")[0]["href"]
+      acc[weight] = FLOW_ROOT + path
+
+      acc
+    end
+  end
+
+  def self.scrape_rankings_for_weight(weight, cached_urls=nil)
+    urls = cached_urls || ranking_urls_by_weight
+    url = urls[weight]
     Wrestler.where(weight: weight).destroy_all
-    url = url_for_weight(weight)
     document = Nokogiri::HTML(DownloadService.download(url))
-    document.css(".oddrow, .evenrow").each do |tr|
-      data = tr.content.split(/(\n|\t)+/).compact
-      name = data[4]
+    document.css(".content.ng-star-inserted tr")[1..].each do |tr|
+      data = tr.css("td").map { |td| td.content }
+      rank, year, name, school, previous = data
       wrestler = Wrestler.find_by(name: name)
-      year = data[8]
-      college = College.find_or_create_by!(name: data[6])
+      college = College.find_or_create_by!(name: school)
       update_data = {
         name: name,
-        rank: data[2],
+        rank: rank,
         college: college,
         weight: weight.to_i,
         year: year,
@@ -37,35 +62,28 @@ class WrestlerService
 
   def self.scrape_team_dual_rankings
     College.update_all(dual_rank: nil)
-    url = "https://intermatwrestle.com/rankings/college/Team2"
+    url = "https://intermatwrestle.com/rankings.html/ncaa-di-r9/"
     document = Nokogiri::HTML(DownloadService.download(url))
-    document.css(".oddrow, .evenrow").each do |tr|
-      data = tr.content.split(/(\n|\t)+/).compact
-      dual_rank = data[2].to_i
-      college = College.find_or_create_by!(name: data[4])
-      college.dual_rank = dual_rank
+    document.css("#dual tr")[2..].each do |tr|
+      data = tr.css("td").map { |td| td.content }
+      rank, school, conference, record, previous = data
+      college = College.find_or_create_by!(name: school)
+      college.dual_rank = rank
       college.save!
     end
   end
 
-  def self.scrape_team_tournament_rankings
+  def self.scrape_team_tournament_rankings(cached_urls=nil)
+    urls = cached_urls || ranking_urls_by_weight
+    url = urls[:tournament]
     College.update_all(tournament_rank: nil)
-    url = "https://intermatwrestle.com/rankings/college/Team"
     document = Nokogiri::HTML(DownloadService.download(url))
-    document.css(".oddrow, .evenrow").each do |tr|
-      data = tr.content.split(/(\n|\t)+/).compact
-      tournament_rank = data[2].to_i
-      college = College.find_or_create_by!(name: data[4])
-      college.tournament_rank = tournament_rank
+    document.css(".content.ng-star-inserted tr")[1..].each do |tr|
+      data = tr.css("td").map { |td| td.content }
+      rank, school, points, previous = data
+      college = College.find_or_create_by!(name: school)
+      college.tournament_rank = rank
       college.save!
     end
-  end
-
-  def self.url_for_weight(weight)
-    if weight == 285
-      weight = "Hwt"
-    end
-
-    "https://intermatwrestle.com/rankings/college/#{weight}"
   end
 end
--- spec/services/wrestler_service_spec.rb
+++ spec/services/wrestler_service_spec.rb
@@ -1,21 +1,71 @@
 require "rails_helper.rb"
 
 describe WrestlerService do
-  describe ".scrape_rankings_for_weight" do
+  describe "current_rankings_url" do
     before do
-      fixture_path = "spec/download_fixtures/intermat_rankings_125.html"
+      fixture_path = "spec/download_fixtures/flow_rankings_index.html"
       fixture_data = File.read(fixture_path)
-      @rankings_125_html = fixture_data
+      @flow_rankings_index = fixture_data
     end
 
-    it "passes the correct URL to DownloadService" do
-      url = "https://intermatwrestle.com/rankings/college/125"
+    it "downloads the rankings index and gets the current NCAA rankings URL" do
       expect(DownloadService)
         .to receive(:download)
-        .with(url)
-        .and_return(@rankings_125_html)
+        .with("https://www.flowrestling.org/rankings")
+        .and_return(@flow_rankings_index)
 
-      WrestlerService.scrape_rankings_for_weight(125)
+      expect(WrestlerService.current_rankings_url).to eq("https://www.flowrestling.org/rankings/10846490-2023-24-ncaa-di-rankings/46726-p4p-carter-starocci")
+    end
+  end
+
+  describe "ranking_urls_by_weight" do
+    before do
+      fixture_path = "spec/download_fixtures/flow_p4p_rankings.html"
+      fixture_data = File.read(fixture_path)
+      @flow_p4p_rankings = fixture_data
+
+      fixture_path = "spec/download_fixtures/flow_rankings_index.html"
+      fixture_data = File.read(fixture_path)
+      @flow_rankings_index = fixture_data
+    end
+
+    it "returns a hash of weights to URLs of rankings" do
+      expect(DownloadService)
+        .to receive(:download)
+        .with("https://www.flowrestling.org/rankings")
+        .and_return(@flow_rankings_index)
+
+      expect(DownloadService)
+        .to receive(:download)
+        .with("https://www.flowrestling.org/rankings/10846490-2023-24-ncaa-di-rankings/46726-p4p-carter-starocci")
+        .and_return(@flow_p4p_rankings)
+
+      result = WrestlerService.ranking_urls_by_weight
+      root = "https://www.flowrestling.org/rankings/10846490-2023-24-ncaa-di-rankings"
+
+      expect(result[125]).to eq("#{root}/46727-125-anthony-noto")
+      expect(result[133]).to eq("#{root}/46728-133-ryan-crookham")
+      expect(result[141]).to eq("#{root}/46729-141-real-woods")
+      expect(result[149]).to eq("#{root}/46730-149-ridge-lovett")
+      expect(result[157]).to eq("#{root}/46731-157-levi-haines")
+      expect(result[165]).to eq("#{root}/46732-165-keegan-otoole")
+      expect(result[174]).to eq("#{root}/46733-174-carter-starocci")
+      expect(result[184]).to eq("#{root}/46734-184-parker-keckeisen")
+      expect(result[197]).to eq("#{root}/46735-197-aaron-brooks")
+      expect(result[285]).to eq("#{root}/46736-285-greg-kerkvliet")
+      expect(result[:tournament]).to eq("#{root}/46737-penn-state")
+
+    end
+  end
+
+  describe ".scrape_rankings_for_weight" do
+    before do
+      fixture_path = "spec/download_fixtures/flow_125_rankings.html"
+      fixture_data = File.read(fixture_path)
+      @rankings_125_html = fixture_data
+      allow(WrestlerService)
+        .to receive(:ranking_urls_by_weight)
+        .and_return({125 => "https://www.flowrestling.org/rankings/10846490-2023-24-ncaa-di-rankings/46727-125-anthony-noto"})
     end
 
     it "creates a wrestler record for each ranked wrestler" do
@@ -29,10 +79,10 @@ describe WrestlerService do
       allow(DownloadService).to receive(:download).and_return(@rankings_125_html)
       WrestlerService.scrape_rankings_for_weight(125)
 
-      wrestler = Wrestler.find_by(name: "Nick Suriano")
-      expect(wrestler.college.name).to eq("Michigan")
+      wrestler = Wrestler.find_by(name: "Anthony Noto")
+      expect(wrestler.college.name).to eq("Lock Haven")
       expect(wrestler.rank).to eq(1)
-      expect(wrestler.year).to eq("Senior")
+      expect(wrestler.year).to eq("JR")
       expect(wrestler.weight).to eq(125)
     end
 
@@ -43,28 +93,28 @@ describe WrestlerService do
         WrestlerService.scrape_rankings_for_weight(125)
       end.to change(Wrestler, :count).by(33)
 
-      expect(Wrestler.where(name: "Nick Suriano").count).to eq(1)
+      expect(Wrestler.where(name: "Anthony Noto").count).to eq(1)
     end
 
     it "updates a wrestler's rank, college, and weight if they have changed" do
       allow(DownloadService).to receive(:download).and_return(@rankings_125_html)
       FactoryBot.create(:wrestler,
-        name: "Nick Suriano",
+        name: "Anthony Noto",
         rank: 17,
         college: FactoryBot.create(:college, name:"Rutgers"),
         weight: 133,
-        year: "Junior",
+        year: "Fr",
       )
 
       WrestlerService.scrape_rankings_for_weight(125)
 
-      expect(Wrestler.where(name: "Nick Suriano").count).to eq(1)
+      expect(Wrestler.where(name: "Anthony Noto").count).to eq(1)
 
-      wrestler = Wrestler.find_by(name: "Nick Suriano")
-      expect(wrestler.college.name).to eq("Michigan")
+      wrestler = Wrestler.find_by(name: "Anthony Noto")
+      expect(wrestler.college.name).to eq("Lock Haven")
       expect(wrestler.rank).to eq(1)
       expect(wrestler.weight).to eq(125)
-      expect(wrestler.year).to eq("Senior")
+      expect(wrestler.year).to eq("JR")
     end
 
     it "creates a college for a new wrestler" do
@@ -96,9 +146,12 @@ describe WrestlerService do
 
   describe ".scrape_team_dual_rankings" do
     before do
-      fixture_path = "spec/download_fixtures/intermat_team_dual_rankings.html"
+      fixture_path = "spec/download_fixtures/intermat_rankings.html"
       fixture_data = File.read(fixture_path)
-      allow(DownloadService).to receive(:download).and_return(fixture_data)
+      expect(DownloadService)
+        .to receive(:download)
+        .with("https://intermatwrestle.com/rankings.html/ncaa-di-r9/")
+        .and_return(fixture_data)
     end
 
     it "updates a teams dual meet ranking" do
@@ -108,7 +161,7 @@ describe WrestlerService do
       WrestlerService.scrape_team_dual_rankings
 
       college.reload
-      expect(college.dual_rank).to eq(11)
+      expect(college.dual_rank).to eq(2)
     end
 
     it "updates a teams dual meet ranking to nil if they aren't on the list" do
@@ -123,19 +176,23 @@ describe WrestlerService do
 
   describe ".scrape_team_tournament_rankings" do
     before do
-      fixture_path = "spec/download_fixtures/intermat_team_tournament_rankings.html"
+      fixture_path = "spec/download_fixtures/flow_team_tournament_rankings.html"
       fixture_data = File.read(fixture_path)
       allow(DownloadService).to receive(:download).and_return(fixture_data)
+
+      allow(WrestlerService)
+        .to receive(:ranking_urls_by_weight)
+        .and_return({tournament: "https://www.flowrestling.org/rankings/10846490-2023-24-ncaa-di-rankings/46737-penn-state"})
     end
 
-    it "updates a teams dual meet ranking" do
+    it "updates a teams tournament ranking" do
       college = FactoryBot.create(:college, name: "Cornell")
       expect(college.tournament_rank).to eq(nil)
 
       WrestlerService.scrape_team_tournament_rankings
 
       college.reload
-      expect(college.tournament_rank).to eq(9)
+      expect(college.tournament_rank).to eq(2)
     end
 
     it "updates a teams tournament ranking to nil if they aren't on the list" do
