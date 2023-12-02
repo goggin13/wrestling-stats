49676a39c85edc271f5fd1b914e2af78dabe540d
Matt Goggin <goggin13@gmail.com>
2023-12-01 18:50:21 -0600

    comment for equivalencies

--- app/models/college.rb
+++ app/models/college.rb
@@ -4,6 +4,7 @@ class College < ApplicationRecord
   has_many :away_matches, class_name: "Match", foreign_key: "away_team_id", dependent: :destroy
 
   EQUIVALENCIES = [
+    # [Canonical, alias_1, alias_2, ...],
     ["UNI", "Northern Iowa"],
     ["App State", "Appalachian State"],
     ["ND State", "North Dakota State"],
