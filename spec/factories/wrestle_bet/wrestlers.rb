4a35fc98608bb75f7ab96724b35dce18a0a20f88
Matt Goggin <goggin13@gmail.com>
2025-03-08 21:28:42 -0600

    updated wrestlebet wrestler factory

--- spec/factories/wrestle_bet/wrestlers.rb
+++ spec/factories/wrestle_bet/wrestlers.rb
@@ -1,6 +1,9 @@
 FactoryBot.define do
   factory :wrestle_bet_wrestler, class: 'WrestleBet::Wrestler' do
-    name { "MyString" }
-    college { nil }
+    sequence :name do |n|
+      "Kyle Dake - #{n}"
+    end
+
+    association :college
   end
 end
